a36b27faa7d7dc3c937529a4ae112dae3ed8fe4a
ViniHGV <ixxvinicius@gmail.com>
2023-12-06 13:50:39 -0300

    feat: adicionando roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreateAcountComponent } from './components/create-acount/create-acount.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'create-account', component: CreateAcountComponent },
+  { path: 'home', component: HomeComponent },
+];
