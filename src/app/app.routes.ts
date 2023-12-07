6a0323ef90a58d4e34f0d0083b18c0f7ea592b44
ViniHGV <ixxvinicius@gmail.com>
2023-12-07 13:55:58 -0300

    feat: adicionando rota com parametro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { LoginPage } from './pages/login/login.component';
 import { CreateAccountPage } from './pages/create-account/create-account.component';
 import { UserProfilePage } from './pages/user-profile/user-profile.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { UserProfileDetailsPage } from './pages/user-profile-details/user-profile-details.component';
 
 export const routes: Routes = [
   { path: '', component: LoginPage },
@@ -12,6 +13,7 @@ export const routes: Routes = [
   { path: 'home', component: HomePage },
   { path: 'newPost', component: NewPostPage },
   { path: 'myaccount', component: UserProfilePage },
+  { path: 'profileUser/:id', component: UserProfileDetailsPage },
   { path: '**', component: PageNotFoundComponent },
   // {path: "search", component: }
 ];
