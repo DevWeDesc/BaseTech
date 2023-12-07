45fafc1c259ccab243f1a7dd3222ad57a0b71e0f
ViniHGV <ixxvinicius@gmail.com>
2023-12-07 13:30:13 -0300

    rafactor: removendo  serviço do swiper

--- src/app/components/swipper-recomended-profiles/swipper-recomended-profiles.component.ts
+++ src/app/components/swipper-recomended-profiles/swipper-recomended-profiles.component.ts
@@ -2,7 +2,6 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import Swiper from 'swiper';
 import { register } from 'swiper/element/bundle';
 import { RecomendedProfilesType } from '../../types/recomended-profiles';
-import { RecomendedProfilesService } from '../../Services/recomended-profiles/recomended-profiles.service';
 register();
 @Component({
   selector: 'app-swipper-recomended-profiles',
@@ -13,16 +12,8 @@ register();
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SwipperRecomendedProfilesComponent implements OnInit {
-  swiper: any = new Swiper('.mySwiper', {});
-
-  recomendedProfiles: RecomendedProfilesType[] = [];
-
-  constructor(private recomendedProfilesData: RecomendedProfilesService) {}
-
-  ngOnInit() {
-    this.recomendedProfilesData.getUsers().subscribe((users) => {
-      this.recomendedProfiles = users;
-    });
-    console.log(this.recomendedProfiles);
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
+  swiper: any = new Swiper('.mySwiper', {});
 }
