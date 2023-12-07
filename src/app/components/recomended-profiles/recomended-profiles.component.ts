497e366532d348ddd50a1a816d311048d410e314
ViniHGV <ixxvinicius@gmail.com>
2023-12-07 13:24:07 -0300

    refactor: refatorando recomended Profiles parqa consumir dados mockados que vem do home

--- src/app/components/recomended-profiles/recomended-profiles.component.ts
+++ src/app/components/recomended-profiles/recomended-profiles.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SwipperRecomendedProfilesComponent } from '../swipper-recomended-profiles/swipper-recomended-profiles.component';
+import { userType } from '../../types/userType';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-recomended-profiles',
   standalone: true,
   templateUrl: './recomended-profiles.component.html',
   styleUrl: './recomended-profiles.component.scss',
-  imports: [SwipperRecomendedProfilesComponent],
+  imports: [SwipperRecomendedProfilesComponent, NgFor],
 })
-export class RecomendedProfilesComponent {}
+export class RecomendedProfilesComponent {
+  @Input() users: userType[] = [];
+}
