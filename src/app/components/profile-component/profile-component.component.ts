80d98dac6c36473eb7cf1468eb68c0c2b5f03efd
ViniHGV <ixxvinicius@gmail.com>
2023-12-07 12:34:37 -0300

    feat: criando o componente profile

--- src/app/components/profile-component/profile-component.component.ts
+++ src/app/components/profile-component/profile-component.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
 
+type optionsProfileMockType = {
+  href: string;
+  text: string;
+  icon: string;
+};
 @Component({
   selector: 'app-profile-component',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './profile-component.component.html',
-  styleUrl: './profile-component.component.scss'
+  styleUrl: './profile-component.component.scss',
 })
-export class ProfileComponentComponent {
-
+export class ProfileComponentComponent implements OnInit {
+  optionsProfileMock: optionsProfileMockType[] = [];
+  ngOnInit() {
+    this.optionsProfileMock = [
+      {
+        href: '',
+        text: 'Detalhes da Conta',
+        icon: '../../../assets/images/book-open.svg',
+      },
+      {
+        href: '',
+        text: 'Editar dados Pessoais',
+        icon: '../../../assets/images/edit.svg',
+      },
+      {
+        href: '/',
+        text: 'Sair dessa Conta',
+        icon: '../../../assets/images/log-out.svg',
+      },
+    ];
+  }
 }
