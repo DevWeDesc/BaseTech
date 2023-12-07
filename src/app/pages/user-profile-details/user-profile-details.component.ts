a5d8b40e99f4ece6f4c6a135417be6bb305b181d
ViniHGV <ixxvinicius@gmail.com>
2023-12-07 13:58:44 -0300

    feat: criando componente de detalhes de perfil dos usuários

---
+++ src/app/pages/user-profile-details/user-profile-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { UserProfileDetailsComponent } from '../../components/user-profile-details-component/user-profile-details.component';
+import { GetUsersService } from '../../Services/get-users.service';
+import { userType } from '../../types/userType';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile-details',
+  standalone: true,
+  templateUrl: './user-profile-details.component.html',
+  imports: [UserProfileDetailsComponent],
+})
+export class UserProfileDetailsPage implements OnInit {
+  constructor(
+    private getUsersMockService: GetUsersService,
+    private route: ActivatedRoute
+  ) {}
+  userSelected: userType | undefined = {
+    id: 0,
+    image: '',
+    name: '',
+    typeAccont: '',
+  };
+  id: string | null | number = this.route.snapshot.paramMap.get('id');
+
+  ngOnInit() {
+    this.getUsersMockService.getUsersMock().subscribe((user) => {
+      this.userSelected = user.find((item) => item.id == this.id);
+    });
+
+    console.log(this.userSelected);
+  }
+}
