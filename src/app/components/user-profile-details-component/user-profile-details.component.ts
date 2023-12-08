401494f33a9da5eb7e2c88c41ccab3d9ffe19033
ViniHGV <ixxvinicius@gmail.com>
2023-12-08 11:00:41 -0300

    feat: adicionando opções e exibição de imagens de postagens

--- src/app/components/user-profile-details-component/user-profile-details.component.ts
+++ src/app/components/user-profile-details-component/user-profile-details.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
-
+import { userType } from '../../types/userType';
+import { ContainerProfileUserComponent } from '../../container-profile-user/container-profile-user.component';
+import { NgIf, NgFor } from '@angular/common';
 @Component({
   selector: 'app-user-profile-details-component',
   standalone: true,
   templateUrl: './user-profile-details.component.html',
   styleUrl: './user-profile-details.component.scss',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    FooterComponent,
+    ContainerProfileUserComponent,
+    NgIf,
+    NgFor,
+  ],
 })
-export class UserProfileDetailsComponent {}
+export class UserProfileDetailsComponent {
+  @Input() userSelected: userType | undefined = {
+    id: 0,
+    image: '',
+    name: '',
+    typeAccont: '',
+    posts: [],
+  };
+  postVisible: boolean = false;
+  handleVisibilityPosts() {
+    this.postVisible ? (this.postVisible = false) : (this.postVisible = true);
+  }
+}
